d55da17f985161fd3ce31338a1e54eee1c19df33
andrepereira2001 <adbp@live.com.pt>
2021-04-21 18:53:38 +0100

    added time checker

--- src/client.c
+++ src/client.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "./include/parse.h"
 
+int generateNumber(int min, int max){
+
+    int number = rand()%(max-min) + min;
+    return number;
+}
+
 int main(int argc, char *argv[]){
+    srand(time(NULL));
     char *fifoname;
     int nsecs;
+    time_t start_time = time(0);
 
     if( parseCommand(argc ,argv , &fifoname , &nsecs) != 0){
         fprintf(stderr, "client: parsing error\n");
         return 1;
+    }
+
+    while(time(0) - start_time < nsecs){
+        sleep(1);
+        printf("hello");
+        fflush(stdout);
     }
 
     return 0;
