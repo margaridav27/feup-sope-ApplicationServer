2faa0b53767f1355ea9782e4e62464ed0b138046
Margarida Vieira <margaridajcv.27@gmail.com>
2021-04-26 16:02:05 +0100

    added updated files

--- src/include/common.h
+++ src/include/common.h
@@ -1,17 +1,10 @@
 #ifndef _COMMON_H
 #define _COMMON_H 1
-
-#include <sys/types.h>
-#include <unistd.h>
-#include <pthread.h>
-
 typedef struct {
-	int rid; 		// request id
-	pid_t pid; 		// process id
-	pthread_t tid;	// thread id
-	int tskload;	// task load
-	int tskres;		// task result
+	int rid; 										// request id
+	pid_t pid; 										// process id
+	pthread_t tid;									// thread id
+	int tskload;									// task load
+	int tskres;										// task result
 } Message;
-
-
 #endif // _COMMON_H
