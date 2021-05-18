d021cbbf93ec8e54489e0ed30e8f08b8941903be
Antonio Ribeiro <up201906761@fe.up.pt>
2021-05-18 01:59:09 +0100

    pointer allocation corrections

--- E2/src/server/server.c
+++ E2/src/server/server.c
@@ -24,7 +24,7 @@
 
 typedef struct node {
 
-    Message *msg; //node message
+    Message msg; //node message
     STAILQ_ENTRY(node) nodes; //next queue node
 
 } node_t;
@@ -55,19 +55,24 @@ int namePrivateFifo(const Message *msg, char *fifo_path) {
     return snprintf(fifo_path, 250, "/tmp/%u.%lu", msg->pid, msg->tid) > 0;
 }
 
+
 int writeToPrivateFifo(const Message *msg) {
     int private;
     char private_fifo[PATH_MAX] = {0};
 
+    
     namePrivateFifo(msg, private_fifo);
 
-    if (fifoExists(private_fifo) != 0){
+    //FIXME: this function should exist
+    /**if (fifoExists(private_fifo) != 0){
         perror("server: fifo does not exist");
         return -1;
-    }
+    }**/
 
     while (remainingTime() > 0 &&
-        (private = open(private_fifo, O_WRONLY | O_NONBLOCK)) < 0) continue;
+        (private = open(private_fifo, O_WRONLY | O_NONBLOCK)) < 0) {
+            continue;
+            }
 
     if (msg == NULL) return -1;
     //pthread_mutex_lock(&write_mutex);
@@ -76,6 +81,7 @@ int writeToPrivateFifo(const Message *msg) {
     const int kmsec = (int) remainingTime() * 1000;
     fd.fd = private;
     fd.events = POLLOUT;
+
     int r = poll(&fd, 1, kmsec);
     if (r < 0) {
         //pthread_mutex_unlock(&write_mutex);
@@ -160,38 +166,38 @@ int writeMessageToStorage(Message *msg){
 
     sem_wait(&empty); // decrease the number of empty slots
 
-    msg->tid = pthread_self();
-    n->msg = msg;
+    //msg->tid = pthread_self();
+    n->msg = *msg;
 
     //insert at end of queue
     STAILQ_INSERT_TAIL(&head, n, nodes);
 
     sem_post(&full); // increase the number of slots with content
 
-    free(n);
-    printf("Written to storage\n");
     return 0;
 }
 
 int readMessageFromStorage(Message *msg){
     node_t *n = malloc(sizeof(*n));
 
-    //if queue is empty return 
-    if(!STAILQ_EMPTY(&head)) 
-        return 1;
-
+    
     sem_wait(&full);// decrease the number of full slots
+
+    /**if(STAILQ_EMPTY(&head)){
+        fprintf(stderr, "Empty queue\n");
+        sem_post(&full);
+        return 1;
+    }**/
 
     n = STAILQ_FIRST(&head);
 
     STAILQ_REMOVE_HEAD(&head, nodes);
 
-    msg = n->msg;
+    *msg = n->msg;
 
     sem_post(&empty); // increase the number of slots without content
 
     free(n);
-    printf("Read to storage\n");
     return 0;
 }
 
@@ -199,7 +205,6 @@ void *consumeTask(void *p){
     Message *msg = malloc(sizeof(*msg));
 
     while (remainingTime() > 0){
-        printf("consumer thread creation \n");
         if(readMessageFromStorage(msg) > 0) continue;
         writeToPrivateFifo(msg);
     }
@@ -317,11 +322,6 @@ int main(int argc, char *argv[]) {
 
     if(bufsz == 0){
        bufsz = BUFSZ_SIZE;
-    }
-
-    if(createStorage() == -1){
-        perror("server: error creating queue");
-        return 1;
     }
 
     sem_init(&empty, 0, bufsz);
